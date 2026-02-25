07e0c0848ea7965e23df25be6c49213b0a12d26c
kartikaychaudhary2107 <chaudharykartikay3@gmail.com>
2026-02-25 10:13:02 +0530

    Resolved merge conflict in PalindromeCheckerApp

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -2,8 +2,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        // Original String
+        // Hardcoded string
         String original = "level";
 
         // Variable to store reversed string
@@ -20,25 +19,5 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
         }
-=======
-        // Hardcoded string literal
-        String word = "madam";
-
-        // Reverse the string manually
-        String reversed = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
-        }
-
-        // Check palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println("The word \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
-        }
-
-        // Program exits automatically after main ends
->>>>>>> 87fcea5db0e9f90b1cedbc9fd88a863ebeda6990
     }
 }
